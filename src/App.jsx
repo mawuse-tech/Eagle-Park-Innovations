f515a5b20629362d8a96a2ad07988f5dd5dff97f
mawuse-tech <akpekemawuse1@gmail.com>
2025-05-31 09:49:19 +0000

    put together the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Navbar/>
     </>
   )
 }
